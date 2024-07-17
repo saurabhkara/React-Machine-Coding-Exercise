c3772945a2940c5d671ea1e3a46ccb2da171661e
Saurabh <saurabkumar679@gmail..com>
2024-07-17 20:48:42 +0530

    17-07-2024

--- 1. Count Down Timer/CountDownTimer/src/App.jsx
+++ 1. Count Down Timer/CountDownTimer/src/App.jsx
@@ -1,35 +1,35 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css";
+import { useState } from "react";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [isStarted, setIsStarted] = useState(false);
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div id="app">
+      <h1 className="title">Count Down Timer</h1>
+      <div className="input-container">
+        <div className="input-box">
+          <input id="hours" placeholder="HH" />
+          <input id="minutes" placeholder="MM" />
+          <input id="seconds" placeholder="SS" />
+        </div>
+        <button className="timer-button">Start</button>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+      <div className="show-container">
+        <div className="timer-box">
+          <div>10</div>
+          <span>:</span>
+          <div>11</div>
+          <span>:</span>
+          <div>50</div>
+        </div>
+        <div className="action-box">
+          <button className="timer-botton">Pause</button>
+          <button className="timer-botton">Reset</button>
+        </div>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    </div>
+  );
 }
 
-export default App
+export default App;
