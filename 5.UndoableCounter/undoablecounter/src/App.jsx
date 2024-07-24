eb9ff29db23b68abfa174d52be7e90d67722febd
Saurabh <saurabkumar679@gmail..com>
2024-07-24 08:39:59 +0530

    24-07-2024

--- 5.UndoableCounter/undoablecounter/src/App.jsx
+++ 5.UndoableCounter/undoablecounter/src/App.jsx
@@ -3,24 +3,86 @@ import "./App.css";
 
 function App() {
   const [count, setCount] = useState(0);
+  const [data, setData] = useState([]);
+  const [redoList, setRedoList] = useState([]);
+
+  const maintainData = (value) => {
+    const obj = { action: value, prev: count, current: count + value };
+    const copyHistory = data;
+    copyHistory.unshift(obj);
+    setData(copyHistory);
+  };
+
+  const handleClick = (value) => {
+    const valueN = parseInt(value);
+    setCount((prev) => prev + valueN);
+    maintainData(valueN);
+  };
+
+  const handleUndo = () => {
+    if (data.length) {
+      const copyHistory = data;
+      const lastAction = copyHistory.shift();
+      console.log(copyHistory);
+      setCount((prev) => prev + lastAction.action);
+      setData(copyHistory);
+    }
+  };
+  const handleRedo = () => {};
 
   return (
     <div className="container">
       <h1 className="title">Undoable Counter</h1>
       <div className="undoRedoBtn">
-        <button>Undo</button>
-        <button>redo</button>
+        <button className="btn" onClick={handleUndo}>
+          Undo
+        </button>
+        <button className="btn" onClick={handleRedo}>
+          redo
+        </button>
       </div>
       <div className="main">
         {[-100, -10, -1].map((value, index) => {
-          return <button key={index}>{value}</button>;
+          return (
+            <button
+              className="btn"
+              key={index}
+              onClick={() => handleClick(value)}
+            >
+              {value}
+            </button>
+          );
         })}
-        <div className="value">0</div>
+        <div className="value">{count}</div>
         {["+1", "+10", "+100"].map((value, index) => {
-          return <button key={index}>{value}</button>;
+          return (
+            <button
+              className="btn"
+              key={index}
+              onClick={() => handleClick(value)}
+            >
+              {value}
+            </button>
+          );
         })}
       </div>
-      <div className="history">History coming...</div>
+      <div className="history">
+        <p className="title">Perfomed Actions</p>
+        <div className="entries">
+          <span>Action</span>
+          <span>Previous</span>
+          <span>Current</span>
+        </div>
+        {data.reverse().map((item, index) => {
+          return (
+            <div key={index} className="entries">
+              <span>{item.action}</span>
+              <span>{item.prev}</span>
+              <span>{item.current}</span>
+            </div>
+          );
+        })}
+      </div>
     </div>
   );
 }
