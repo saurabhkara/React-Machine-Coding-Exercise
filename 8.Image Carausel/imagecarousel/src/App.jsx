c9e8f5cd3b25f1ce2ddaa5ae2d9228ececb0b49e
Saurabh <saurabkumar679@gmail..com>
2024-08-01 07:48:49 +0530

    01-08-2024

--- 8.Image Carausel/imagecarousel/src/App.jsx
+++ 8.Image Carausel/imagecarousel/src/App.jsx
@@ -1,14 +1,63 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0);
+  const [imgData, setImgData] = useState([]);
+  const [index, setIndex] = useState(0);
+
+  const fetchImages = async () => {
+    const url = "https://www.reddit.com/r/aww/top/.json?t=all";
+    const res = await fetch(url);
+    const result = await res.json();
+    const data = result.data.children;
+    const list = data.filter((item) =>
+      item.data.url_overridden_by_dest.includes("jpg")
+    );
+    setImgData(list);
+    console.log(list);
+  };
+
+  useEffect(() => {
+    fetchImages();
+  }, []);
+
+  const handleClick = (dir) => {
+    const lastIndex = imgData.length - 1;
+    if (dir === "left") {
+      if (index === 0) {
+        setIndex(lastIndex);
+      } else {
+        setIndex((prev) => prev - 1);
+      }
+    } else {
+      if (index === lastIndex) {
+        setIndex(0);
+      } else {
+        setIndex((prev) => prev + 1);
+      }
+    }
+  };
+
+  useEffect(() => {
+    const sid = setInterval(() => {
+      handleClick("right");
+    }, 3000);
+    return () => {
+      clearInterval(sid);
+    };
+  }, [index]);
 
   return (
     <div className="app">
-      <button>{"<"}</button>
-      <img src="" alt="image not found" />
-      <button>{">"}</button>
+      <button onClick={() => handleClick("left")}>{"<"}</button>
+      <img
+        src={imgData[index]?.data.url}
+        alt="image not found"
+        className="img"
+      />
+      <button className="right" onClick={() => handleClick("right")}>
+        {">"}
+      </button>
     </div>
   );
 }
