e8804d609e283efdb582157a038e484ea3b3c4d4
Saurabh <saurabkumar679@gmail..com>
2024-08-16 18:23:41 +0530

    16-08-2024

--- 12.Product Filter/productfilter/src/App.jsx
+++ 12.Product Filter/productfilter/src/App.jsx
@@ -1,35 +1,71 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect, useState } from "react";
+import "./App.css";
+import { items } from "./item";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const filters = ["Bags", "Watches", "Sports", "Sunglasses"];
+  const [filterItem, setFilterItem] = useState(items);
+  const [activeFilter, setActiveFilter] = useState([]);
+
+  const handleFilterButtonClick = (e) => {
+    const category = e.target.id;
+    if (!category) {
+      return;
+    }
+    console.log(e.target.id);
+    if (activeFilter.includes(category)) {
+      //remove from active filter
+      const filteredCategory = activeFilter.filter((item) => item !== category);
+      setActiveFilter(filteredCategory);
+    } else {
+      //add into active filter
+      const activeFilterCopy = [...activeFilter, category];
+      setActiveFilter(activeFilterCopy);
+    }
+  };
+
+  const applyFilterItem = () => {
+    if (activeFilter.length) {
+      const filteredItemResult = items.filter((item) =>
+        activeFilter.includes(item.category)
+      );
+      setFilterItem(filteredItemResult);
+    } else {
+      setFilterItem(items);
+    }
+  };
+
+  useEffect(() => {
+    applyFilterItem();
+  }, [activeFilter]);
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className="app">
+      <div className="filters" onClick={handleFilterButtonClick}>
+        {filters.map((item, index) => {
+          return (
+            <button
+              key={index}
+              id={item}
+              className={activeFilter.includes(item) ? "selected" : null}
+            >
+              {item}
+            </button>
+          );
+        })}
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+      <div className="product-list">
+        {filterItem.map((itm, idx) => {
+          return (
+            <div key={idx} className="item">
+              <p>{itm.name}</p>
+              <p className="category">{itm.category}</p>
+            </div>
+          );
+        })}
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    </div>
+  );
 }
 
-export default App
+export default App;
